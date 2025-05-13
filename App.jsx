7292838cc010763b03152b12ead764e8fc293ae5
Jay Prajapati <jay.prajapati.gj@gmail.com>
2025-05-13 18:24:28 +0530

    Update App.jsx
    removed semicolleon

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 
 
 
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Pokemon } from './Pokemon'; // Your main Pokémon component
 //import PokemonDetail from './PokemonDetail'; // Your detail component
